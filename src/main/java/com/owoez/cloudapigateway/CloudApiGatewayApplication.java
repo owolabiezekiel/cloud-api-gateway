e84976341513e96ce8347bf433dc96a7a5bf1a8d
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-08-06 13:45:45 +0100

    Setup api gateway to route requests. done in the application.yml file. also registers as eureka client

--- src/main/java/com/owoez/cloudapigateway/CloudApiGatewayApplication.java
+++ src/main/java/com/owoez/cloudapigateway/CloudApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.owoez.cloudapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudApiGatewayApplication {
 
   public static void main(String[] args) {
